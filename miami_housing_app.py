8b9d3d8d946b68000b4f639818097467e3c4639f
Code.manu <164360071+codeflex98@users.noreply.github.com>
2024-11-15 03:29:48 +0100

    Update miami_housing_app.py

--- miami_housing_app.py
+++ miami_housing_app.py
@@ -10,9 +10,9 @@ def main():
     st.title("Miami Housing Prediction by Manvanth")
 
     p1 = st.number_input("LATITUDE")
-    p2 = st.number_input('LONGITUDE')
+    p2 = st.number_input("LONGITUDE")
 
-    p3 = st.number_input(LND_SQFOOT)
+    p3 = st.number_input("LND_SQFOOT")
     p4 = st.number_input("TOT_LVG_AREA")
     p5 = st.number_input("SPEC_FEAT_VAL")
 
